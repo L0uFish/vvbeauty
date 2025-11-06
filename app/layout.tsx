cbd220dafb71f6a7549a88f08c61b38fd2619685
L0uFish <wouterslouie@gmail.com>
2025-11-06 23:54:44 +0100

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,22 @@ import "./globals.css";
 import { UserProvider } from "@/app/context/UserContext";
 import { AuthUIProvider } from "@/app/context/AuthUIContext";
 
+export const metadata = {
+  title: "VVBeauty",
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.ico", rel: "shortcut icon" },
+      { url: "/apple-touch-icon.png", sizes: "180x180", rel: "apple-touch-icon" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
+  other: {
+    "apple-mobile-web-app-title": "VVBeauty",
+  },
+};
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="nl">
